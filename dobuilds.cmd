2172375c436204a82a4ee0bc9f819581afcc8e54
James Dunne <james.jdunne@gmail.com>
2013-02-20 18:14:20 -0600

    Updated build script to be branch-aware.

--- dobuilds.cmd
+++ dobuilds.cmd
@@ -1,39 +1,46 @@
 @echo off
+SET BRANCH=32bpp-persdiv
+
 SET MSBUILD=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
 SET ZIP7="C:\Program Files\7-zip\7z.exe"
 
 PUSHD \git\odamex\
 
+echo Checking out branch %BRANCH%
+"C:\cygwin\bin\git.exe" checkout -f %BRANCH%
+
 echo Determining git revision...
-SET REVISION=%1
-"C:\cygwin\bin\git.exe" rev-list --abbrev-commit -n 1 32bpp >revs.lst
+SET REVISION=
+"C:\cygwin\bin\git.exe" rev-list --abbrev-commit -n 1 HEAD >revs.lst
 FOR /f %%a IN (revs.lst) DO (
   IF NOT DEFINED REVISION SET REVISION=%%a
 )
 DEL /F /Q revs.lst
 echo Git revision %REVISION%
 
-SET ZIPBASE=odamex-32bpp-%REVISION%
+SET ZIPBASE=%BRANCH%-%REVISION%
 
 echo Building 64-bit release...
+
 PUSHD \git\odamex\build64\client
 %MSBUILD% odamex.vcxproj /m /p:Configuration=RelWithDebInfo
 CD RelWithDebInfo
 COPY ..\..\..\README-32bpp .
 DEL /F %ZIPBASE%-x64.zip
 %ZIP7% a %ZIPBASE%-x64.zip "odamex.exe" "README-32bpp"
-"C:\cygwin\bin\scp.exe" "%ZIPBASE%-x64.zip" bit:/home/ftp/jim/software/odamex/
+"C:\cygwin\bin\scp.exe" "%ZIPBASE%-x64.zip" bit:/home/ftp/jim/software/odamex/%BRANCH%/
 REM START .
 POPD
 
 echo Building 32-bit release...
+
 PUSHD \git\odamex\build32\client
 %MSBUILD% odamex.vcxproj /m /p:Configuration=RelWithDebInfo
 CD RelWithDebInfo
 COPY ..\..\..\README-32bpp .
 DEL /F %ZIPBASE%-x86.zip
 %ZIP7% a %ZIPBASE%-x86.zip "odamex.exe" "README-32bpp"
-"C:\cygwin\bin\scp.exe" "%ZIPBASE%-x86.zip" bit:/home/ftp/jim/software/odamex/
+"C:\cygwin\bin\scp.exe" "%ZIPBASE%-x86.zip" bit:/home/ftp/jim/software/odamex/%BRANCH%/
 REM START .
 POPD
 
